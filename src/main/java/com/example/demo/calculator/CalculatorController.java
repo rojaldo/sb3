103f52041e7b62ff7cb7a1702de2d9e319efb69b
Daniel Rojo Pérez <danielrojo@gmail.com>
2024-07-02 13:14:00 +0200

    calculator state machine

--- src/main/java/com/example/demo/calculator/CalculatorController.java
+++ src/main/java/com/example/demo/calculator/CalculatorController.java
@@ -13,15 +13,34 @@ public class CalculatorController {
     private CalculatorService calculatorService;
 
     @GetMapping("/calculator")
-    public String getMethodName(@RequestParam(name = "n1", required = false, defaultValue = "3") float op1,
-            @RequestParam(name = "n2", required = false, defaultValue = "2") float op2,
-            @RequestParam(name = "op", required = false, defaultValue = "*") String operation,
+    public String getMethodName(@RequestParam(name = "n1", required = false, defaultValue = "0") float op1,
+            @RequestParam(name = "n2", required = false , defaultValue = "0") float op2,
+            @RequestParam(name = "op", required = false, defaultValue = "") String operation,
+            @RequestParam(name = "eval", required = false, defaultValue = "") String eval,
             Model view) {
+                if (!eval.equals("") &&  op1 == 0 && op2 == 0 && operation.equals("")) {
+                    String evalStr = this.calculatorService.process(eval);
+                    view.addAttribute("res", this.calculatorService.getResult());
+                    view.addAttribute("num1", this.calculatorService.getFirstNumber());
+                    view.addAttribute("num2", this.calculatorService.getSecondNumber());
+                    view.addAttribute("op", this.calculatorService.getOperation());
+                    view.addAttribute("correct", evalStr.equals("ERROR") ? false : true);
+                    return "calculator";
+                    
+                }else if (eval.equals("") ) {
+                    view.addAttribute("res", 0);
+                    view.addAttribute("num1", 0);
+                    view.addAttribute("num2", 0);
+                    view.addAttribute("op", "");
+                    view.addAttribute("correct", true);
+                    return "calculator";
+                }
         float result = this.calculatorService.calculate(op1, op2, operation);
         view.addAttribute("res", result);
         view.addAttribute("num1", op1);
         view.addAttribute("num2", op2);
         view.addAttribute("op", operation);
+        view.addAttribute("correct", true);
         return "calculator";
     }
 
--- src/main/java/com/example/demo/calculator/CalculatorService.java
+++ src/main/java/com/example/demo/calculator/CalculatorService.java
@@ -2,9 +2,40 @@ package com.example.demo.calculator;
 
 import org.springframework.stereotype.Service;
 
+enum CalculatorState {
+    INIT,
+    FIRST_NUMBER,
+    SECOND_NUMBER,
+    RESULT,
+    ERROR
+}
+
 @Service
 public class CalculatorService {
-    
+
+    CalculatorState currentState = CalculatorState.INIT;
+    float firstNumber = 0;
+    float secondNumber = 0;
+    String operation = "";
+    float result = 0;
+    String resultString = "";
+
+    public float getFirstNumber() {
+        return firstNumber;
+    }
+
+    public float getSecondNumber() {
+        return secondNumber;
+    }
+
+    public String getOperation() {
+        return operation;
+    }
+
+    public float getResult() {
+        return result;
+    }
+
     public float calculate(float op1, float op2, String operation) {
         float result = 0;
         switch (operation) {
@@ -24,5 +55,101 @@ public class CalculatorService {
                 break;
         }
         return result;
+    }
+
+    public String process(String eval) {
+        // read each character
+        // if it is a number, call processNumber
+        // if it is an symbol, call processSymbol
+
+        for (int i = 0; i < eval.length(); i++) {
+            char c = eval.charAt(i);
+            if (Character.isDigit(c)) {
+                // cast char to int
+                processNumber(Character.getNumericValue(c));
+            } else {
+                processSymbol(c);
+            }
+
+        }
+        if (this.currentState == CalculatorState.RESULT) {
+            this.currentState = CalculatorState.INIT;
+            return this.resultString;
+        } else {
+            this.currentState = CalculatorState.INIT;
+            return "ERROR";
+        }
+    }
+
+    void clearCalculator() {
+        this.currentState = CalculatorState.INIT;
+        this.firstNumber = 0;
+        this.secondNumber = 0;
+        this.operation = "";
+        this.result = 0;
+        this.resultString = "";
+    }
+
+    void processNumber(int number) {
+        switch (this.currentState) {
+            case INIT:
+                this.clearCalculator();
+                this.firstNumber = number;
+                this.currentState = CalculatorState.FIRST_NUMBER;
+                break;
+            case FIRST_NUMBER:
+                this.firstNumber = this.firstNumber * 10 + number;
+                break;
+            case SECOND_NUMBER:
+                this.secondNumber = this.secondNumber * 10 + number;
+                break;
+            case RESULT:
+                this.currentState = CalculatorState.ERROR;
+                break;
+            case ERROR:
+            this.resultString = "ERROR";
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    void processSymbol(char c) {
+        switch (this.currentState) {
+            case INIT:
+                this.clearCalculator();
+                this.currentState = CalculatorState.ERROR;
+                break;
+            case FIRST_NUMBER:
+                if (c == '+' || c == '-' || c == '*' || c == '/') {
+                    this.operation = String.valueOf(c);
+                    this.currentState = CalculatorState.SECOND_NUMBER;
+                } else {
+                    this.currentState = CalculatorState.ERROR;
+                }
+                break;
+            case SECOND_NUMBER:
+                if (c == '=') {
+                    this.result = calculate(this.firstNumber, this.secondNumber, this.operation);
+                    this.resultString = String.valueOf(this.result);
+                    this.currentState = CalculatorState.RESULT;
+                } else {
+                    this.currentState = CalculatorState.ERROR;
+                }
+                break;
+            case RESULT:
+                this.currentState = CalculatorState.ERROR;
+                break;
+            case ERROR:
+                this.resultString = "ERROR";
+                break;
+            default:
+                break;
+        }
+    }
+
+    public boolean isCorrect() {
+        return this.currentState == CalculatorState.INIT;
     }
 }
